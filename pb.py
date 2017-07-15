eb642912440f84f32ace17930576e06e9e20783c
Phixion <ms@phixion.org>
2017-07-15 15:52:23 +0200

    added dummy nicks

--- pb.py
+++ pb.py
@@ -3,7 +3,7 @@ import sopel
 
 @sopel.module.commands('pushbullet','pb','alarm','push')
 def pushbullet(bot, trigger):
-    if trigger.nick == 'Phixion' or trigger.nick == 'vespertine':
+    if trigger.nick == 'BOTOWNER_NICK' or trigger.nick == 'BOTADMIN_NICK':
         url = trigger.group(2)
-        pb = Pushbullet('YOUR_API_KEY)
+        pb = Pushbullet('YOUR_API_KEY')
         pb.push_link(url,url)
