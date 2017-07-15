1a3f48032a7b52d352ef6a70a51acc626c4b5dbf
Phixion <ms@phixion.org>
2017-07-15 16:33:27 +0200

    don't really need that

--- pb.py
+++
@@ -1,9 +0,0 @@
-from pushbullet import Pushbullet
-import sopel
-
-@sopel.module.commands('pushbullet','pb','alarm','push')
-def pushbullet(bot, trigger):
-    if trigger.nick == 'BOTOWNER_NICK' or trigger.nick == 'BOTADMIN_NICK':
-        url = trigger.group(2)
-        pb = Pushbullet('YOUR_API_KEY')
-        pb.push_link(url,url)
