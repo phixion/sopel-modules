c530521aa2450ff689ecaec78babb28b67ec5373
Phixion <ms@phixion.org>
2017-07-18 15:03:32 +0200

    ability to trigger url too

--- porncomment.py
+++ porncomment.py
@@ -3,10 +3,20 @@ import requests
 
 @sopel.module.commands('yp')
 @sopel.module.example('.yp trump')
-def youporn(bot, trigger):
+def porncomment(bot, trigger):
   search_for = trigger.group(2)
   header = {'accept': 'application/json'}
   rpc = requests.get("https://porncomment.com",{'search': search_for}, headers=header).json()["comments"]
   comment = rpc[0]['body']
   bot.say(comment, max_messages=2)
-  
+
+@sopel.module.commands('ypurl')
+@sopel.module.example('.ypurl trump')
+def porncomment_url(bot, trigger):
+  search_for = trigger.group(2)
+  header = {'accept': 'application/json'}
+  rpc = requests.get("https://porncomment.com",{'search': search_for}, headers=header).json()["comments"]
+  comment = rpc[0]['body']
+  url = rpc[0]['source_url']
+  full_comment = '%s - %s' %(comment, url)
+  bot.say(full_comment, max_messages=2)
