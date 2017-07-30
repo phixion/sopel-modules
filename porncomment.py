69d35a2456c471000b5ac897529e5464ad387055
Phixion <ms@phixion.org>
2017-07-30 16:26:38 +0200

    added errormessages

--- porncomment.py
+++ porncomment.py
@@ -6,17 +6,23 @@ import requests
 def porncomment(bot, trigger):
   search_for = trigger.group(2)
   header = {'accept': 'application/json'}
-  rpc = requests.get("https://porncomment.com",{'search': search_for}, headers=header).json()["comments"]
-  comment = rpc[0]['body']
-  bot.say(comment, max_messages=2)
+  try:
+    rpc = requests.get("https://porncomment.com", {'search': search_for}, headers=header).json()["comments"]
+    comment = rpc[0]['body']
+    bot.say(comment, max_messages=2)
+  except IndexError:
+    bot.say("no comment found :(")
 
 @sopel.module.commands('ypurl')
 @sopel.module.example('.ypurl trump')
 def porncomment_url(bot, trigger):
   search_for = trigger.group(2)
   header = {'accept': 'application/json'}
-  rpc = requests.get("https://porncomment.com",{'search': search_for}, headers=header).json()["comments"]
-  comment = rpc[0]['body']
-  url = rpc[0]['source_url']
-  full_comment = '%s - %s' %(comment, url)
-  bot.say(full_comment, max_messages=2)
+  try:
+    rpc = requests.get("https://porncomment.com", {'search': search_for}, headers=header).json()["comments"]
+    comment = rpc[0]['body']
+    url = rpc[0]['source_url']
+    full_comment = '%s - %s' %(comment, url)
+    bot.say(full_comment, max_messages=2)
+  except IndexError:
+    bot.say("no comment found :(")
