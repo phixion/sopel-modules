2df24450108401947437f82fa8c3cc58225e0046
Phixion <ms@phixion.org>
2017-10-08 08:48:05 +0200

    save currency values

--- cur.py
+++ cur.py
@@ -1,23 +1,14 @@
+# Flexible crypto currency module
+# Created by @sc0tt (https://github.com/sc0tt)
 import requests
 import sys
 import json
 import sopel
 
-last_prices = {}
+with open('/home/bot/.sopel/cur_py_cache') as read_file:
+  last_prices = json.load(read_file)
 main_coins = ["btc", "bch", "xrp", "eth"]
 single_url = "https://api.bitfinex.com/v1/pubticker/{0}usd"
-#multi_url = "http://api.cryptocoincharts.info/tradingPairs"
-
-# @sopel.module.rule('^\.(\w+)2(\w+)\s?(.+)?$')
-# def crypto_exchange(bot, trigger):
-  # from_cur = trigger.group(1)
-  # to_cur = trigger.group(2)
-  # quantity = float(trigger.group(3)) if trigger.group(3) is not None else 1
-
-  # api_result = requests.get(single_url.format(from_cur, to_cur)).json()
-  # calc_result = quantity * float(api_result["price"])
-
-  # bot.say("{0} {1} is about {2:.4f} {3}".format(quantity, api_result["coin1"], float(calc_result), api_result["coin2"]))
 
 @sopel.module.rule('^\.({0})$'.format("|".join(main_coins)))
 def crypto_spot(bot, trigger):
@@ -31,10 +22,12 @@ def crypto_spot(bot, trigger):
 
   if from_cur not in last_prices:
     last_prices[from_cur] = 0
-
-  diffStr = getDiffString(float(api_result["last_price"]), last_prices[from_cur])
+  digits = False if from_cur.lower()=='xrp' else True
+  diffStr = getDiffString(float(api_result["last_price"]), last_prices[from_cur], digits)
   last_prices[from_cur] = float(api_result["last_price"])
-  bot.say("{0}: ${1:.4f}{2}".format(from_cur, float(api_result["last_price"]), diffStr))
+  with open('/home/bot/.sopel/cur_py_cache', 'w') as outfile:
+    json.dump(last_prices, outfile)
+  bot.say("{0}: ${1:.{2}f}{3}".format(from_cur, float(api_result["last_price"]), 2 if digits else 4, diffStr))
 
 @sopel.module.commands('ticker','tick')
 def tick(bot, trigger):
@@ -44,17 +37,23 @@ def tick(bot, trigger):
     api_result = requests.get(single_url.format(coin)).json()
     if coin not in last_prices:
       last_prices[coin] = 0
-    diffStr = getDiffString(float(api_result["last_price"]), last_prices[coin])
+    digits = False if coin.lower() == 'xrp' else True
+    diffStr = getDiffString(float(api_result["last_price"]), last_prices[coin], digits)
     last_prices[coin] = float(api_result["last_price"])
-    results.append("{0}: ${1:.4f}{2}".format(coin, float(api_result["last_price"]), diffStr))
+    results.append("{0}: ${1:.{2}f}{3}".format(coin, float(api_result["last_price"]), 2 if digits else 4, diffStr))
+  with open('/home/bot/.sopel/cur_py_cache', 'w') as outfile:
+    json.dump(last_prices, outfile)
   bot.say(" | ".join(results))
 
 def getDiffString(current_price, last_price, crates=False):
+  global last_prices
   diff = current_price - last_price
   diffStr = ""
   if diff != 0:
     sign = "+" if diff > 0 else ''
     diffStr = " ({0}{1:.{2}f})".format(sign, diff, 2 if crates else 4)
+  with open('/home/bot/.sopel/cur_py_cache', 'w') as outfile:
+    json.dump(last_prices, outfile)
   return diffStr
 
 def getSteamMarketPrice(item):
@@ -77,4 +76,7 @@ def crates(bot, trigger):
   results = []
   for crate in crates:
     results.append(getSteamMarketPrice(crate))
+  with open('/home/bot/.sopel/cur_py_cache', 'w') as outfile:
+    json.dump(last_prices, outfile)
   bot.say(" | ".join(results))
+  
